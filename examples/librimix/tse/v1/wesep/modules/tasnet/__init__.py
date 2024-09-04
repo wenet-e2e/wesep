0b75e3beeb1f7a863b3d79f8d1d5abceff1ac903
cdliang11 <1404056823@qq.com>
2024-09-04 23:01:52 +0800

    [lint] add pre-commit

--- examples/librimix/tse/v1/wesep/modules/tasnet/__init__.py
+++
@@ -1,6 +0,0 @@
-from wesep.modules.tasnet.decoder import DeepDecoder
-from wesep.modules.tasnet.decoder import MultiDecoder
-from wesep.modules.tasnet.encoder import DeepEncoder
-from wesep.modules.tasnet.encoder import MultiEncoder
-from wesep.modules.tasnet.separation import Separation, FuseSeparation
-from wesep.modules.tasnet.speaker import ResNet4SpExplus
